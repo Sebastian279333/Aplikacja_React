6e752ab3825d941f2de5c931f748cb2f88731b50
Sebastian279333 <sebkewalec@gmail.com>
2025-11-21 19:07:00 +0100

    Like Counter defines local state for number of Likes

--- src/components/APP/LikeCounter/LikeCounter.js
+++ src/components/APP/LikeCounter/LikeCounter.js
@@ -1,11 +1,33 @@
-import './LikeCounter.css'
+import { useState } from 'react';
+import './LikeCounter.css';
+
 function LikeCounter() {
+    const [likes, setLikes] = useState(0);
+    const handleLike = () => {
+        setLikes(likes + 1);
+    };
+    const handleDisLike = () => {
+        if (likes < 1) {
+            return;
+        }
+        setLikes(likes - 1);
+    };
+
+    const counterStyle = {
+        color: likes > 5 ? "red" : "black",
+        fontWeight: "bold",
+        fontSize: "20px",
+    }
 
     return (
-        <div className = 'post'>
-            <p>Fajny post! Licz lików X</p>
-            <button>Polub 👍</button>
+        <div className='post'>
+            <p style={counterStyle}>Fajny post! Licz lików {likes}</p>
+            <div className='actions'>
+                <button onClick={handleLike}>Polub 👍</button>
+                <button onClick={handleDisLike}>DisLike 👍</button>
+            </div>
         </div>
-    )
+    );
 }
-export default LikeCounter
+
+export default LikeCounter;
